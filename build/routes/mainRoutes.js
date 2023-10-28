761769a68d0f0d54b045d0480b18de2faaad2546
raihanmd <mraihanf471@gmail.com>
2023-10-28 09:47:39 +0700

    compiled success

--- build/routes/mainRoutes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import userController from "../controller/userController.js";
-const router = Router();
-router.post("/api/register", userController.register);
-router.post("/api/login", userController.login);
-export { router };
